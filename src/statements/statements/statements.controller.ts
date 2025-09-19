ba1efe6cbe87f528ab407a24570bcf8782582f93
ayan <ayanfront@livin.kz>
2025-09-19 16:15:16 +0500

    commit all minio functionality

--- src/statements/statements/statements.controller.ts
+++ src/statements/statements/statements.controller.ts
@@ -1,35 +1,34 @@
-import {
-  Controller,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { MinioService } from 'src/minio/minio/minio.service';
-import { Express } from 'express';
+// import {
+//   Controller,
+//   Post,
+//   UploadedFile,
+//   UseInterceptors,
+// } from '@nestjs/common';
+// import { FileInterceptor } from '@nestjs/platform-express';
+// import { Express } from 'express';
 
-@Controller('statements')
-export class StatementsController {
-  constructor(private readonly minioService: MinioService) {}
+// @Controller('statements')
+// export class StatementsController {
+//   constructor(private readonly minioService: MinioService) {}
 
-  @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadStatement(@UploadedFile() file: Express.Multer.File) {
-    if (!file) {
-      throw new Error('Файл не передан');
-    }
+//   @Post('upload')
+//   @UseInterceptors(FileInterceptor('file'))
+//   async uploadStatement(@UploadedFile() file: Express.Multer.File) {
+//     if (!file) {
+//       throw new Error('Файл не передан');
+//     }
 
-    const fileName = await this.minioService.uploadStatement(
-      file.buffer,
-      file.originalname,
-      file.mimetype,
-    );
-    console.log('File uploaded:', fileName);
+//     const fileName = await this.minioService.uploadStatement(
+//       file.buffer,
+//       file.originalname,
+//       file.mimetype,
+//     );
+//     console.log('File uploaded:', fileName);
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const fileUrl = await this.minioService.getFileUrl(fileName);
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//     const fileUrl = await this.minioService.getFileUrl(fileName);
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    return { fileName, fileUrl };
-  }
-}
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//     return { fileName, fileUrl };
+//   }
+// }
