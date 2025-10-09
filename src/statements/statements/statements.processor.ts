953cac55a2ffefafac19285c4eda0047ac0fc85b
ayan <ayanfront@livin.kz>
2025-10-09 17:02:09 +0500

    add worker

--- src/statements/statements/statements.processor.ts
+++ src/statements/statements/statements.processor.ts
@@ -1,26 +1,19 @@
-import { WorkerHost } from '@nestjs/bullmq';
-import { Logger } from '@nestjs/common';
+import { OnWorkerEvent, WorkerHost } from '@nestjs/bullmq';
+import { Logger, OnModuleInit } from '@nestjs/common';
 import { Job } from 'bullmq';
 
-export class StatementsProcessor extends WorkerHost {
+export class StatementsProcessor extends WorkerHost implements OnModuleInit {
   private readonly logger = new Logger(StatementsProcessor.name);
+
+  onModuleInit() {
+    this.logger.log(
+      '🚀 StatementsProcessor initialized and connected to Redis',
+    );
+  }
+
   async process(job: Job<any>) {
     this.logger.log(`📄 Processing job ${job.name} with ID ${job.id}`);
-    this.logger.debug(`Payload: ${JSON.stringify(job.data)}`);
-
-    try {
-      // 1️⃣ Скачать PDF из S3 (fileUrl)
-      // 2️⃣ Передать в AI-сервис (позже добавим)
-      // 3️⃣ Получить JSON
-      // 4️⃣ Сохранить результат в Redis или в Postgres (по сценарию)
-
-      // Временно просто лог:
-      this.logger.log(`✅ Job ${job.id} processed successfully`);
-      return { status: 'ok' };
-    } catch (err) {
-      this.logger.error(`❌ Job ${job.id} failed`, err);
-      throw err;
-    }
+    return { status: 'ok' };
   }
 
   @OnWorkerEvent('failed')
