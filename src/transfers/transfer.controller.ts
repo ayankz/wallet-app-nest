88310073f7d3071eb1fb4aea9370dada79e891e3
ayankz <ayanpost87@gmail.com>
2026-07-09 16:10:42 +1200

    Add transfer controller

--- src/transfers/transfer.controller.ts
+++ src/transfers/transfer.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { TransferService } from './transfer.service';
+import { GetCurrentUserId } from 'src/common/decorators';
+import { CreateTransferDto } from './dto/create-transfer.dto';
 
-@Controller('transfer')
-export class TransferController {}
+@Controller('transfers')
+@ApiTags('transfers')
+@ApiBearerAuth()
+export class TransferController {
+    constructor(private transferService: TransferService) { }
+    @Post()
+    createTransfer(
+        @GetCurrentUserId() userId: number,
+        @Body() dto: CreateTransferDto,
+    ) {
+        return this.transferService.createTransfer(userId, dto);
+    }
+}
+
